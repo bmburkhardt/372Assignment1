cf41be3e013a8d2c40527d968cf482faef674d57
bmburkhardt <bmburkhardt@alaska.edu>
2017-01-23 20:09:41 -0900

    Added line to end of file on remote file

--- assignment1.cpp
+++ assignment1.cpp
@@ -1,3 +1,4 @@
 Hi,
 My name is Bryan Burkhardt and I'm creating a file for assignment 1. 
 Hopefully we can create some major conflict problems!
+I'm also adding something to the end of the file on the remote file.
